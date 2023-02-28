a425ca0086173e140f7f78e32743c68b91ce0e45
ajiths10 <ajithsnair10@gmail.com>
2023-02-28 22:38:19 +0530

    Insertion Sort attached

--- arrayMaper.ts
+++ arrayMaper.ts
@@ -1,26 +1,41 @@
 /**
- * Arrange the given array in single array.
+ * Arrange the given array in single array sorted.
  * */
 
-let arrQ = [2, 5, [6, 3, [1, 8], 7], [14, 15], 4];
+let arrQ = [2, 11, 5, [6, 3, [1, 8], 7], [14, 15], 4];
 
-type maper = (number | (number | number[])[])[];
+type mapertype = (number | (number | number[])[])[];
 
-const maper = (runner: maper) => {
+//Insertion Sort
+const InsertionSort = (buscat: number[]) => {
+  for (let i = 0; i < buscat.length; i++) {
+    for (let j = i; j < buscat.length; j++) {
+      if (buscat[i] > buscat[j]) {
+        [buscat[i], buscat[j]] = [buscat[j], buscat[i]];
+      }
+    }
+  }
+  return buscat;
+};
+
+//make into single array
+const maper = (runner: mapertype) => {
   let tempstorage = [];
   for (let i = 0; i < runner.length; i++) {
     if (typeof runner[i] === "object") {
-      let collect: maper = maper(runner[i] as maper);
+      let collect: mapertype = maper(runner[i] as mapertype);
       tempstorage.push(...collect);
     } else {
       tempstorage.push(runner[i]);
     }
   }
-  return tempstorage;
+
+  return InsertionSort(tempstorage as number[]);
 };
 
 let res = maper(arrQ);
 console.log(res);
 /**
- * Array maper
+ * Chnage to single array.
+ * Sort array
  * */
