7f97efb86a5180e99442f7cfe5bacfd534ceadf3
ajiths10 <ajithsnair10@gmail.com>
2023-03-18 15:43:44 +0530

    optimise with set()

--- Array/ContainesDuplicate.ts
+++ Array/ContainesDuplicate.ts
@@ -32,3 +32,17 @@ console.log(containsDuplicate([1, 2, 3, 1]));
 console.log(containsDuplicate([1, 2, 3, 4]));
 console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
 console.log(containsDuplicate([]));
+
+/**
+ * @example with Set()
+ */
+
+const containsDuplicateTwo = function (nums: number[]) {
+  const set = new Set(nums);
+  return set.size !== nums.length;
+};
+
+console.log("with set()", containsDuplicateTwo([1, 2, 3, 1]));
+console.log("with set()", containsDuplicateTwo([1, 2, 3, 4]));
+console.log("with set()", containsDuplicateTwo([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+console.log("with set()", containsDuplicateTwo([]));
