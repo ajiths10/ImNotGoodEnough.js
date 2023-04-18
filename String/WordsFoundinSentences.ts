4aeb12925216b7f6d6adb25b6200bebb5d73c78a
ajiths10 <ajithsnair10@gmail.com>
2023-04-18 20:28:17 +0530

    value is not being reassigned

--- String/WordsFoundinSentences.ts
+++ String/WordsFoundinSentences.ts
@@ -26,7 +26,7 @@ const mostWordsFound = (sentences: string[]): number => {
   let count = 0;
 
   for (let i = 0; i < sentences.length; i++) {
-    let temp_length = sentences[i].split(" ").length;
+    const temp_length = sentences[i].split(" ").length;
     if (temp_length > count) {
       count = temp_length;
     }
