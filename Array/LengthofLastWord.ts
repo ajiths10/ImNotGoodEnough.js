808e4293c90eab20634eea966261a4206ecb776f
Ajiths10 <ajithsnair10@gmail.com>
2023-05-30 22:24:56 +0530

    more example added | leecode 58

--- Array/LengthofLastWord.ts
+++ Array/LengthofLastWord.ts
@@ -8,6 +8,15 @@
  * Output: 5
  * Explanation: The last word is "World" with length 5.
  * 
+ * @example 2
+ * Input: s = "   fly me   to   the moon  "
+ * Output: 4
+ * Explanation: The last word is "moon" with length 4.
+ * 
+ * @example 3
+ * Input: s = "luffy is still joyboy"
+ * Output: 6
+ * Explanation: The last word is "joyboy" with length 6.
 */
 
 
@@ -26,3 +35,6 @@ const lengthOfLastWord = function(s: string) {
 };
 
 console.log(lengthOfLastWord("Hello World"))
+console.log(lengthOfLastWord("   fly me   to   the moon  "))
+console.log(lengthOfLastWord("luffy is still joyboy"))
+console.log(lengthOfLastWord("Today is a nice day"))
